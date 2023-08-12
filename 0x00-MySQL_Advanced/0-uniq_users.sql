4d334d3b6bfc884a3add6bfbcfbd78ecbf3303c0
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-12 19:13:02 +0200

    unique user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Uniquie Users
-CREATE TABLE  IF NOT EXISTS 'users' (
+CREATE TABLE  IF NOT EXISTS `users` (
     id int NOT NULL AUTO_INCREMENT PRIMARY_KEY,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255)
