4fa224297ce00aeaa3717b6873f91284324de8b0
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-15 09:53:21 +0200

    unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
 -- script that creates a table users with follwing fields
 -- id, email, name
-CREATE DATABASE IF NOT EXISTS `holberton`;
-USE `holberton`;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
