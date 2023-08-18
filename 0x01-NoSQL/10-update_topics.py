96c50128ccf08e1f857e9a45e60d9404732c9161
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-18 13:43:02 +0200

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Change School Topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update school topics
+    Args:
+        mongo_collection(collection object):
+
+        name(string) will be the school name to update
+
+        topics (list of strings) will be the list of topics
+    """
+    filter = {"name": name}
+    data = {
+        "$set": {
+            "topics": topics
+            }
+        }
+    updated = mongo_collection.update_one(filter, data)
+    return updated
