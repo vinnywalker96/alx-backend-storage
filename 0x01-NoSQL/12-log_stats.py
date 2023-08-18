a4f84c62638832bcebd9e059d19c53a45c286981
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-18 18:08:26 +0200

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""Nginx logs"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    data_nginx = list(collection.find())
-    method = {
-            "GET": 0,
-            "POST": 0,
-            "PUT": 0,
-            "PATCH": 0,
-            "DELETE": 0,
-            "path": 0
-        }
+def get_logs_stats(collection):
+    """Return log stats
 
-    for logs in data_nginx:
-        if logs['method'] == "GET":
-            method['GET'] += 1
-        if logs['method'] == "POST":
-            method['POST'] += 1
-        if logs['method'] == "PUT":
-            method['PUT'] += 1
-        if logs['method'] == "PATCH":
-            method['PATCH'] += 1
-        if logs['method'] == "DELETE":
-            method['DELETE'] += 1
-        if logs['path'] == '/status':
-            method['path'] += 1
+    Args:
+        collection
+    """
+    number_of_logs = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    total_logs = {
+            method: collection.count_documents(
+                {
+                    "method": method
+                }) for method in methods}
+
+    status_check = collection.count_documents(
+        {
+            "method": "GET",
+            "path": "/status"
+        })
+    return (number_of_logs, total_logs, status_check)
+
+
+def print_stats(number_of_logs, total_logs, status_check):
+    """Print stats    
+    Args:
+
+        number_of_logs()
+
+        total_logs()
+
+        status_check()
+
+    """
     print("Methods:")
-    print(f"    method GET: {method['GET']}")
-    print(f"    method POST: {method['POST']}")
-    print(f"    method PUT: {method['PUT']}")
-    print(f"    method PATCH: {method['PATCH']}")
-    print(f"    method DELETE: {method['DELETE']}")
-    print(f"{method['path']} status check") 
-    client.close()
+    for method, count in total_logs.items():
+        print(f"\t{method}: {count}")
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    with MongoClient('mongodb://127.0.0.1:27017') as client:
+        db = client.logs.nginx
+        number_of_logs, total_logs, status_check = get_logs_stats(db)
+        print_stats(number_of_logs, total_logs, status_check)
