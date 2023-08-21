4b26bde13b83c3335536fa397fb545af0bb69a63
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-21 18:09:36 +0200

    example

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Redis"""
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+
+    def __init__(self) -> None:
+        """Create instance of
+        class Cache
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[
+          str, int, float, bytes]) -> str:
+        """store val to redis server"""
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
