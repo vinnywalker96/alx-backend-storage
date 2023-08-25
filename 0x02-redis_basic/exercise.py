35a1f465a41d72a0262e1931ca4fdc7476f1b091
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-25 15:35:38 +0200

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -37,14 +37,3 @@ class Cache:
         return int(value)
 
 
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
