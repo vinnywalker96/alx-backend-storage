42ad0233c100a08631e8f5dc845ad8f4e32f4ad8
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-25 15:33:33 +0200

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Redis"""
-from typing import Union
+from typing import Union, Callable
 import redis
 import uuid
 
@@ -20,3 +20,31 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn=None):
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if str(value) is not str:
+            return self.get_str(value)
+        else:
+            return self.get_str(value)
+
+    def get_str(self, value):
+        return str(value)
+
+    def get_int(self, value):
+        return int(value)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
