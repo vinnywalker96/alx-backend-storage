112679b55c81873e26931193c8f60efcf7c5d5ab
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-29 06:18:06 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,12 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(
 	IN user_id INT)
 BEGIN
-	DECLARE average_score DECIMAL(10, 2)
-	SELECT AVG(score) INTO average_score FROM corrections WHERE id = user_id LIMIT 1;
+	DECLARE avg_score DECIMAL(10, 2)
+	SELECT AVG(score) INTO avg_score
+	FROM score
+	WHERE id = user_id;
+
+	UPDATE users SET average_score = avg_score
+	WHERE id = user_id
 END //
 DELIMITER ;
