1a84d6a69f626d42cfc054e5ad695d921971833a
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-29 06:24:37 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,15 @@
--- Calculates Average
+-- Script that creates a stored procedure ComputeAverageScoreForUser
 
-DELIMITER $$
+
+DELIMITER $$ ;
 CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id INT)
+	IN user_id INT
+)
 BEGIN
 	UPDATE users
-	SET average_score(SELECT AVG(score) FROM corrections
-		WHERE id=user_id);
-END; $$
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+
+END;$$
 DELIMITER ;
