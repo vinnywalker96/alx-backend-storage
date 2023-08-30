3e8c93881958ed8cee913c57dd9bd0e077bf3b19
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-30 07:49:54 +0000

    cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,31 +20,34 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get_str(self, data: bytes) -> str:
-        val = data.decode("utf-8")
-        return val
-
-    def get_int(self, data: bytes) -> int:
-        return int(data)
-
-    def get(self, key: str, fn: Callable = None) -> bytes:
+    def get(self, key: str, fn: Callable = None):
         val = self._redis.get(key)
         if val is None:
             return None
         if fn is None:
             return None
         return fn(val)
-    
+
     def get_str(self, key: str) -> str:
-        """Convert byte to str"""
         val = self._redis.get(key)
         return val.decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        """Convert byte str to int"""
         val = self._redis.get(key)
         try:
             val = int(val.decode("utf-8"))
         except Exception as err:
             vale = 0
         return val
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
