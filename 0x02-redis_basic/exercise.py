d1ef9656a7a6356592f9f3a2d1871bc3f999711e
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-31 09:59:47 +0000

    Storing list

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,6 +15,17 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input = str(*args)
+        self._redis.rpush(f'{method.__qualname__}:inputs', input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(f'{method.__qualname__}:outputs', output)
+        return output
+    return wrapper
+        
+
 class Cache:
     def __init__(self) -> None:
         """Create instance of
@@ -24,6 +35,7 @@ class Cache:
         self._redis.flushdb()
     
     @count_calls
+    @call_history
     def store(self, data: [
           str, bytes, int, float]) -> str:
         """store val to redis server"""
